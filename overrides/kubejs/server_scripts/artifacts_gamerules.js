252d77f1e9a35d41d94984d4abc4f42cc0bfbca4
adam <57104144+adam9899@users.noreply.github.com>
2025-07-27 18:22:19 +0100

    added a time set day on world creation
    fixes weird daytime monster bug

--- overrides/kubejs/server_scripts/artifacts_gamerules.js
+++ overrides/kubejs/server_scripts/artifacts_gamerules.js
@@ -2,7 +2,8 @@
 ServerEvents.loaded(event => {
 if(event.world.persistentData.artifactsGamerulesSet) return;
   event.server.runCommandSilent('gamerule artifacts.snorkel.isInfinite false');
-  event.server.runCommandSilent('gamerule artifacts.vampiricGlove.maxHealingPerHit 2');
+  event.server.runCommandSilent('gamerule artifacts.vampiricGlove.maxHealingPerHit 3');
   event.server.runCommandSilent('gamerule artifacts.feralClaws.attackSpeedBonus 20');
+  event.server.runCommandSilent('time set day');
 event.world.persistentData.artifactsGamerulesSet = true
 })
